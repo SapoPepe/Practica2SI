56978c1ba96130959af68c61f9d9f14c8083c07d
Juan Carlos Blanco Martínez <143273845+JU4NC4RR@users.noreply.github.com>
2025-02-18 17:10:27 +0100

    Database Creation

--- Practica1/main.py
+++ Practica1/main.py
@@ -1,8 +1,45 @@
 import sqlite3
 import json
+def leerJSON():
+    ficheroJSON = open('datos.json', 'r')
+    datos = json.load(ficheroJSON)
+    print()
+    #print(datos)
+    #print(datos["tickets_emitidos"])
+    #print(datos["tipos_incidentes"])
 
-ficheroJSON = open('datos.json', 'r')
-datos = json.load(ficheroJSON)
-print(datos)
-print("Fichero Leido")
+
+def crearBBDD():
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+
+
+    cur.execute("CREATE TABLE IF NOT EXISTS tickets_emitidos ("
+                "")
+
+
+    cur.execute("CREATE TABLE IF NOT EXISTS clientes ("
+                "id_cli INTEGER,"
+                "nombre TEXT,"
+                "provincia TEXT,"
+                "telefono TEXT"
+                ");")
+
+    cur.execute("CREATE TABLE IF NOT EXISTS empleados ("
+                "fecha_contrato TEXT,"
+                "id_emp INTEGER PRIMARY KEY,"
+                "nivel INTEGER,"
+                "nombre TEXT"
+                ");")
+
+    cur.execute("CREATE TABLE IF NOT EXISTS tipos_incidentes ("
+                "id_cli INTEGER,"
+                "nombre TEXT PRIMARY KEY"
+                ");")
+
+    cur.execute("INSERT INTO tipos_incidentes(id_cli, nombre) VALUES ('1', 'Manolo')")
+
+leerJSON()
+crearBBDD()
+
 
